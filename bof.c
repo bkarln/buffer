2292a735d7171d63e3cf2da52106c59ccc434530
Karolaine Brandelero <55720260+bkarln@users.noreply.github.com>
2022-12-15 02:01:21 -0300

    Create bof.c

---
+++ bof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char ** argv)
+{
+    char buffer[5];
+    strcpy(buffer, argv[1]);
+    return 0;
+}
+[15/12/2022 01:58] sus@SAS:~/Documents/Ret2libc
